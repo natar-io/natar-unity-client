b2389aa02ec8120baaead08be89e09b54bd7cc2c
Nicolas PALARD <palardnicolas@gmail.com>
2018-06-05 15:44:27 +0200

    :sparkles: Started to get image data from redis

--- Assets/RedisConnection.cs
+++ Assets/RedisConnection.cs
@@ -1,57 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using UnityEngine;
 // using StackExchange.Redis;
 using TeamDev.Redis;
 
 public class RedisConnection : MonoBehaviour {
-
-//    ConnectionMultiplexer redis;
+    
+    // Set this as a public variable so it can be edited from Unity Editor
+    public String key = "costum:image";
 
     private RedisDataAccessProvider redis;
-    private string[] keys;
+    private Texture2D videoTexture;
     
-    void Start () {
-	Debug.Log("Redis Connection start");
-	
-	redis = new RedisDataAccessProvider ();
-	redis.Configuration.Host = "127.0.0.1";
-	redis.Configuration.Port = 6379;
-	redis.Connect ();
-	redis.SendCommand (RedisCommand.KEYS, "*");
-	keys = redis.ReadMultiString ();
-	for (int i = 0; i < keys.Length; i++) {
-	    redis.SendCommand (RedisCommand.GET, keys [i]);
-	    string value = redis.ReadString ();
-	    Debug.Log(i.ToString()+" "+ keys[i] + ":" + value);
-	}
+    void Start() {
+        Debug.Log("Redis Connection start");
 
-    // Create the texture
+        redis = new RedisDataAccessProvider ();
+        redis.Configuration.Host = "127.0.0.1";
+        redis.Configuration.Port = 6379;
+        redis.Connect();
+
+        RedisImageToTexture();
     }
 
-    Texture2D videoTexture;
-    
-    void createTexture(){
+    void RedisImageToTexture()
+    {
+        // Get this particular commandId
+        int commandId = redis.SendCommand(RedisCommand.GET, key);
+        // Get image data from this particular command to avoid unexpected results
+        string imageData = RedisTryReadString(commandId);
 
-    // Create a new 2x2 texture ARGB32 (32 bit with alpha) and no mipmaps
-         var videoTexture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
-         
-         // set the pixel values
-         videoTexture.SetPixel(0, 0, new Color(1.0f, 1.0f, 1.0f, 0.5f));
-         videoTexture.SetPixel(1, 0, Color.clear);
-         videoTexture.SetPixel(0, 1, Color.white);
-         videoTexture.SetPixel(1, 1, Color.black);
-         
-         // Apply all SetPixel calls
-         videoTexture.Apply();
-         
-         // connect texture to material of GameObject this script is attached to
+        commandId = redis.SendCommand(RedisCommand.GET, key + ":width");
+        Debug.Log("Getting " + key + ":width");
+        int? width = RedisTryReadInt(commandId);
 
-         GetComponent<Renderer>().material.mainTexture = videoTexture;
+        commandId = redis.SendCommand(RedisCommand.GET, key + ":height");
+        int? height = RedisTryReadInt(commandId);
+
+        if (!width.HasValue || !height.HasValue)
+        {
+            throw new ArgumentException("Could not create image from data: invalid width or height");
+        }
+
+        Debug.Log("Image:" + (int)width + "x" + (int)height);
+
+        videoTexture = new Texture2D((int)width, (int)height, TextureFormat.ARGB32, false);
+        
+        videoTexture.SetPixel(0, 0, new Color(1.0f, 1.0f, 1.0f, 0.5f));
+        videoTexture.SetPixel(1, 0, Color.clear);
+        videoTexture.SetPixel(0, 1, Color.white);
+        videoTexture.SetPixel(1, 1, Color.black);
+
+        videoTexture.Apply();
+        this.GetComponent<Renderer>().material.mainTexture = videoTexture;
     }
-	
+    
     // Update is called once per frame
-    void Update () {
-	
+    void Update() {
+
+    }
+
+    // Helper function to read int from redis server
+    int? RedisTryReadInt(int commandId)
+    {
+        string value = null;
+        value = RedisTryReadString(commandId);
+        int val;
+        if (Int32.TryParse(value, out val)) {
+            return val;
+        }
+        return null;
+    }
+
+    // Helper function to read string from redis server
+    string RedisTryReadString(int commandId)
+    {
+        string value = null;
+        try {
+            value = redis.ReadString(commandId);
+        } catch (Exception e) {
+            Debug.LogError("Failed read string.");
+        }
+        return value;
     }
 }
