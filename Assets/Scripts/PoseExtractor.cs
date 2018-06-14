f7aa322f88acb7ea90eec64c79436c8444eb338d
Nicolas PALARD <palardnicolas@gmail.com>
2018-06-14 16:01:15 +0200

    :sparkles: Better seethrough app

--- Assets/Scripts/PoseExtractor.cs
+++ Assets/Scripts/PoseExtractor.cs
@@ -1,35 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+using TeamDev.Redis;
 using UnityEngine;
 
 public class PoseExtractor : MonoBehaviour {
 
 	public string PoseKey = "custom:image:detected-pose";
+
+	public bool poseUpdated = true;
+	public Matrix4x4 pose3D = new Matrix4x4();
 	// Use this for initialization
 	void Start () {
-		Matrix4x4? pose3D = Utils.RedisTryGetPose3D (RedisConnectionHandler.Instance.redis, PoseKey);
-		if (pose3D.HasValue) {
-			ApplicationParameters.pose3D = (Matrix4x4) pose3D;
-			Debug.Log ((Matrix4x4) pose3D);
-			Debug.Log (Utils.ExtractTranslation ((Matrix4x4) pose3D));
-			this.transform.position = Utils.ExtractTranslation ((Matrix4x4) pose3D);
-			this.transform.rotation = Utils.ExtractRotation ((Matrix4x4) pose3D);
-		}
+		RedisConnectionHandler.Instance.redis.ChannelSubscribed += new ChannelSubscribedHandler (OnChannelSubscribed);
+		RedisConnectionHandler.Instance.redis.MessageReceived += new MessageReceivedHandler (OnMessageRecieved);
+		RedisConnectionHandler.Instance.redis.Messaging.Subscribe (PoseKey);
 	}
 
-	void Update () {
-		Matrix4x4? pose3D = Utils.RedisTryGetPose3D (RedisConnectionHandler.Instance.redis, PoseKey);
-		if (pose3D.HasValue) {
-			ApplicationParameters.pose3D = (Matrix4x4) pose3D;
-			Debug.Log ((Matrix4x4) pose3D);
-			Debug.Log (Utils.ExtractTranslation ((Matrix4x4) pose3D));
+	void OnChannelSubscribed (string channelName) {
+		Debug.Log ("[POSE] Successfully subscribe to: " + channelName);
+	}
+
+	void OnMessageRecieved (string channelName, string message) {
+		if (channelName != PoseKey) {
+			return;
+		}
+		poseUpdated = false;
+		pose3D = Utils.JSONToPose3D (message);
+		Debug.Log(pose3D);
+	}
+
+	void Update()
+	{
+		if (!poseUpdated)
+		{
 			this.transform.position = Utils.ExtractTranslation ((Matrix4x4) pose3D);
-			/*
-			Vector3 position = this.transform.position;
-			position.x = -position.x;
-			this.transform.position = position;
-			*/
 			this.transform.rotation = Utils.ExtractRotation ((Matrix4x4) pose3D);
+			poseUpdated = true;
 		}
 	}
 }
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -7,19 +7,18 @@ using UnityEngine; // Color32, Texture2D
 using TeamDev.Redis; // RedisDataAccessProvider
 
 public static class Utils {
-    
+
     /// <summary>
     /// Tries to read redis command output as an int.
     /// </summary>
     /// <param name="redis"></param>
     /// <param name="commandId">the command to read the output</param>
     /// <returns>the int if success, else null</returns>
-    public static int? RedisTryReadInt(RedisDataAccessProvider redis, int commandId)
-    {
+    public static int? RedisTryReadInt (RedisDataAccessProvider redis, int commandId) {
         string value = null;
-        value = RedisTryReadString(redis, commandId);
+        value = RedisTryReadString (redis, commandId);
         int val;
-        if (Int32.TryParse(value, out val)) {
+        if (Int32.TryParse (value, out val)) {
             return val;
         }
         return null;
@@ -31,14 +30,12 @@ public static class Utils {
     /// <param name="redis"></param>
     /// <param name="commandId">the command to read the output</param>
     /// <returns>the string if success, else null.</returns>
-    public static string RedisTryReadString(RedisDataAccessProvider redis, int commandId)
-    {
+    public static string RedisTryReadString (RedisDataAccessProvider redis, int commandId) {
         string value = null;
         try {
-            value = redis.ReadString(commandId);
-        } 
-        catch (Exception e) {
-            Debug.LogError("Failed read string.");
+            value = redis.ReadString (commandId);
+        } catch (Exception e) {
+            Debug.LogError ("Failed read string.");
         }
         return value;
     }
@@ -49,14 +46,12 @@ public static class Utils {
     /// <param name="redis"></param>
     /// <param name="commandId">the command to read the output from</param>
     /// <returns></returns>
-    public static byte[] RedisTryReadData(RedisDataAccessProvider redis, int commandId)
-    {
+    public static byte[] RedisTryReadData (RedisDataAccessProvider redis, int commandId) {
         byte[] data = null;
         try {
-            data = redis.ReadData(commandId);
-        } 
-        catch (Exception e) {
-            Debug.Log("Failed to read data.");
+            data = redis.ReadData (commandId);
+        } catch (Exception e) {
+            Debug.Log ("Failed to read data.");
         }
         return data;
     }
@@ -66,12 +61,11 @@ public static class Utils {
     /// </summary>
     /// <param name="key">the key where to look for parameters</param>
     /// <returns></returns>
-    public static CameraParameters RedisTryGetCameraParameters(RedisDataAccessProvider redis, string key)
-    {
-        int commandId = redis.SendCommand(RedisCommand.GET, key);
-        string cameraParameters = Utils.RedisTryReadString(redis, commandId);
+    public static CameraParameters RedisTryGetCameraParameters (RedisDataAccessProvider redis, string key) {
+        int commandId = redis.SendCommand (RedisCommand.GET, key);
+        string cameraParameters = Utils.RedisTryReadString (redis, commandId);
         if (cameraParameters != null) {
-            CameraParameters camParams = JsonUtility.FromJson<CameraParameters>(cameraParameters);
+            CameraParameters camParams = JsonUtility.FromJson<CameraParameters> (cameraParameters);
             return camParams;
         }
         return null;
@@ -82,41 +76,45 @@ public static class Utils {
     /// </summary>
     /// <param name="key">the key whereto look for 3d pose"</param>
     /// <returns></returns>
-    public static Matrix4x4? RedisTryGetPose3D(RedisDataAccessProvider redis, string key)
-    {
-        int commandId = redis.SendCommand(RedisCommand.GET, key);
-        string jsonPose = Utils.RedisTryReadString(redis, commandId);
+    public static Matrix4x4? RedisTryGetPose3D (RedisDataAccessProvider redis, string key) {
+        int commandId = redis.SendCommand (RedisCommand.GET, key);
+        string jsonPose = Utils.RedisTryReadString (redis, commandId);
         if (jsonPose != null) {
             // Hand made parsing
-            var charstoRemove = new string[] { ",", "[", "]", "\n", "\t"};
-            foreach (var c in charstoRemove) {
-                jsonPose = jsonPose.Replace(c, string.Empty);
-            }
-            Matrix4x4 poseMatrix = new Matrix4x4();
-            string[] poseValue = jsonPose.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-            int cpt = 0;
-            foreach (var value in poseValue) {
-                if (cpt >= 16)
-                    cpt = cpt - 16 + 1;
-                poseMatrix[cpt] = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat);
-                cpt += 4;
-            }
-            return poseMatrix;
+            return JSONToPose3D (jsonPose);
         }
         return null;
-    }   
+    }
+
+    public static Matrix4x4 JSONToPose3D (string message) {
+        var charstoRemove = new string[] { ",", "[", "]", "\n", "\t" };
+        foreach (var c in charstoRemove) {
+            message = message.Replace (c, string.Empty);
+        }
+        Matrix4x4 poseMatrix = new Matrix4x4 ();
+        string[] poseValue = message.Split (new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+        int cpt = 0;
+        for (int i = 0 ; i < 4 ; i++)
+        {
+            float val0 = float.Parse(poseValue[i*4], CultureInfo.InvariantCulture.NumberFormat);
+            float val1 = float.Parse(poseValue[i*4+1], CultureInfo.InvariantCulture.NumberFormat);
+            float val2 = float.Parse(poseValue[i*4+2], CultureInfo.InvariantCulture.NumberFormat);
+            float val3 = float.Parse(poseValue[i*4+3], CultureInfo.InvariantCulture.NumberFormat);
+            poseMatrix.SetRow(i, new Vector4 (val0, val1, val2, val3));
+        }
+        return poseMatrix;
+    }
 
     /// <summary>
     /// Convert raw image data (byte array) into Color32 array.
     /// </summary>
     /// <param name="data">Raw image data</param>
     /// <returns></returns>
-    public static Color32[] ByteArrayToColor(byte[] data)
-    {
-        Color32[] colorData = new Color32[data.Length/3];
-        for (int i = 0 ; i < data.Length ; i += 3) {
-            Color32 value = new Color32(data[i], data[i+1], data[i+2], 255);
-            colorData[i/3] = value;
+    public static Color32[] ByteArrayToColor (byte[] data) {
+        Color32[] colorData = new Color32[data.Length / 3];
+        for (int i = 0; i < data.Length; i += 3) {
+            Color32 value = new Color32 (data[i], data[i + 1], data[i + 2], 255);
+            colorData[i / 3] = value;
         }
         return colorData;
     }
@@ -129,26 +127,24 @@ public static class Utils {
     /// <param name="cy">the y position</param>
     /// <param name="r">the circle radius</param>
     /// <param name="col">the circle color</param>
-    public static void Circle(Texture2D tex, int cx, int cy, int r, Color col) {
-         int x, y, px, nx, py, ny, d;
-         
-         for (x = 0; x <= r; x++)
-         {
-             d = (int)Mathf.Ceil(Mathf.Sqrt(r * r - x * x));
-             for (y = 0; y <= d; y++)
-             {
-                 px = cx + x;
-                 nx = cx - x;
-                 py = cy + y;
-                 ny = cy - y;
- 
-                 tex.SetPixel(px, py, col);
-                 tex.SetPixel(nx, py, col);
-  
-                 tex.SetPixel(px, ny, col);
-                 tex.SetPixel(nx, ny, col);
-             }
-         }    
+    public static void Circle (Texture2D tex, int cx, int cy, int r, Color col) {
+        int x, y, px, nx, py, ny, d;
+
+        for (x = 0; x <= r; x++) {
+            d = (int) Mathf.Ceil (Mathf.Sqrt (r * r - x * x));
+            for (y = 0; y <= d; y++) {
+                px = cx + x;
+                nx = cx - x;
+                py = cy + y;
+                ny = cy - y;
+
+                tex.SetPixel (px, py, col);
+                tex.SetPixel (nx, py, col);
+
+                tex.SetPixel (px, ny, col);
+                tex.SetPixel (nx, ny, col);
+            }
+        }
     }
 
     /// <summary>
@@ -156,46 +152,25 @@ public static class Utils {
     /// </summary>
     /// <param name="matrix">the 4x4 Matrix</param>
     /// <returns></returns>
-    public static Quaternion ExtractRotation(Matrix4x4 matrix)
-    {
-        Vector3 forward;
-        forward.x = matrix.m02;
-        forward.y = matrix.m12;
-        forward.z = matrix.m22;
- 
-        Vector3 upwards;
-        upwards.x = matrix.m01;
-        upwards.y = matrix.m11;
-        upwards.z = matrix.m21;
- 
-        return Quaternion.LookRotation(forward, upwards);
+    public static Quaternion ExtractRotation (Matrix4x4 matrix) {
+        return Quaternion.LookRotation (matrix.GetColumn (2), matrix.GetColumn (1));
     }
- 
+
     /// <summary>
     /// Extract Translation from a 4x4 matrix
     /// </summary>
     /// <param name="matrix">the 4x4 Matrix</param>
     /// <returns></returns>
-    public static Vector3 ExtractTranslation(Matrix4x4 matrix)
-    {
-        Vector3 position;
-        position.x = matrix.m03;
-        position.y = matrix.m13;
-        position.z = matrix.m23;
-        return position;
+    public static Vector3 ExtractTranslation (Matrix4x4 matrix) {
+        return matrix.GetColumn (3);
     }
-    
+
     /// <summary>
     /// Extract Scale from a 4x4 matrix
     /// </summary>
     /// <param name="matrix">the 4x4 Matrix</param>
     /// <returns></returns>
-    public static Vector3 ExtractScale(Matrix4x4 matrix)
-    {
-        Vector3 scale;
-        scale.x = new Vector4(matrix.m00, matrix.m10, matrix.m20, matrix.m30).magnitude;
-        scale.y = new Vector4(matrix.m01, matrix.m11, matrix.m21, matrix.m31).magnitude;
-        scale.z = new Vector4(matrix.m02, matrix.m12, matrix.m22, matrix.m32).magnitude;
-        return scale;
+    public static Vector3 ExtractScale (Matrix4x4 matrix) {
+        return new Vector3 (matrix.GetColumn (0).magnitude, matrix.GetColumn (1).magnitude, matrix.GetColumn (2).magnitude);
     }
 }
